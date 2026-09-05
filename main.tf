1e49543555b12bfa5614f523fc581de1ea4eb947
ashisnishanka <ak.nishanka@gmail.com>
2026-09-05 08:46:07 +0530

    added terraform cicd

---
+++ main.tf
@@ -0,0 +1,41 @@
+terraform {
+  #backend block where terraform statefile will store 
+  #it will store in s3 bucket and it is called remote statefile
+  backend "s3" {
+   bucket = "terraform-idream-tffile-bk"
+   key = "dev/terraform.tfstate"
+   region = "ap-south-1"
+   encrypt = true
+   #use_lockfile = true
+}
+#provider block 
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "ap-south-1"
+}
+# it will create s3 bucket in aws 
+resource "aws_s3_bucket" "idream-bk" {
+bucket = "idreamit-bucket-1234567"
+ tags = {
+  Name = "Qa-bucket-idream"
+  Environment = "Qa"
+ }
+}
+# it will create linux-os  in aws 
+resource "aws_instance" "mylinuxos"{
+ ami = "ami-name"
+ instance_type = "t2.micro"
+ tags = {
+    Name = "Qa_mylinuxos"
+	Environment = "var.environment"
+ }
+ 
+}
